a90edbc7dda90f00404b8807dcf4ca31dbb8f99b
teranum <tera63614@gmail.com>
2023-12-16 21:39:44 +0900

    Add DevCenter

--- eBEST.OpenApi.DevCenter/App.xaml.cs
+++ eBEST.OpenApi.DevCenter/App.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using App.Helpers;
+using CommunityToolkit.Mvvm.DependencyInjection;
+using eBEST.OpenApi.DevCenter.ViewModels;
+using eBEST.OpenApi.DevCenter.Views;
+using Microsoft.Extensions.DependencyInjection;
 using System.Windows;
+using System.Windows.Input;
 
 namespace eBEST.OpenApi.DevCenter
 {
@@ -9,6 +13,30 @@ namespace eBEST.OpenApi.DevCenter
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            Ioc.Default.ConfigureServices(
+                new ServiceCollection()
+
+                //Services
+                .AddSingleton<IAppRegistry>(new AppRegistry("teranum"))
+                .AddSingleton<MainViewModel>()
+
+                .BuildServiceProvider()
+                );
+
+            Startup += (sender, args) =>
+            {
+                var mainView = new MainView();
+                mainView.MouseDown += (sender, e) =>
+                {
+                    if (e.ChangedButton == MouseButton.Left)
+                        mainView.DragMove();
+                };
+                mainView.DataContext = Ioc.Default.GetRequiredService<MainViewModel>();
+                mainView.Show();
+            };
+        }
     }
 
 }
